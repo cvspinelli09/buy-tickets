0b6bfe23780f4512928a69d6dfa5a69ae381dc39
cvspinelli <cvspinelli09@gmail.com>
2020-05-06 19:58:22 +0100

    Updates

---
+++ precache-manifest.60d182cb2fa2c2d59dd0d3fe8ca25924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f88b500c40339bc8fc089f99cc4bb0",
+    "url": "/buy-tickets/index.html"
+  },
+  {
+    "revision": "1ab1a8f7637c00774991",
+    "url": "/buy-tickets/static/css/main.58849494.chunk.css"
+  },
+  {
+    "revision": "cbb21db977d641173120",
+    "url": "/buy-tickets/static/js/2.5e8e9dcd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/buy-tickets/static/js/2.5e8e9dcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab1a8f7637c00774991",
+    "url": "/buy-tickets/static/js/main.a3298b03.chunk.js"
+  },
+  {
+    "revision": "7f25fc51bb747574e25f",
+    "url": "/buy-tickets/static/js/runtime-main.419a40fb.js"
+  }
+]);
